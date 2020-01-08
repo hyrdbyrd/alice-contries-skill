b59760a2e8833602e961724a6b7121453d056dca
hyrdbyrd <ilya777grin23@yandex-team.ru>
2020-01-08 17:27:06 +0300

    feat

--- index.js
+++ index.js
@@ -1,40 +1,23 @@
 const { json } = require('micro');
 
-const { contries } = require('./lib/contries');
+const { toLowerCase } = require('./lib/helpers');
+
+const { chat } = require('./lib/chat');
+
+const db = {};
 
 module.exports = async (req, res) => {
-    if (req.method !== 'POST') {
-        res.statusCode = 404;
-        return res.end('Not found :(');
-    }
-
     const { request, session, version } = await json(req);
 
-    const helloMessage =
-        'Привет! Я помогу вам подтянуть ваши знания в географии!' +
-        'Назовите, пожалуйста, интересующий континент.' +
-        'Если вам не важно какой какой континент учить, скажи "Земля".' +
-        'Если вам надоест, просто скажи "Алиса, Хватит", и я тот час остановлюсь.';
-
-    let question = request.original_utterance && contries(request);
-    question = question.question || 'Хм, что-то пошло нет так.';
+    const chatState = db[session.session_id] || (db[session.session_id] = { state: 'START' });
 
     res.end(JSON.stringify(
         {
             version,
             session,
             response: {
-                // В свойстве response.text возвращается исходная реплика пользователя.
-                // Если навык был активирован без дополнительной команды,
-                // пользователю нужно сказать "Hello!".
-                text:
-                    request.original_utterance
-                        ? helloMessage
-                        : question,
-
-                // Свойство response.end_session возвращается со значением false,
-                // чтобы диалог не завершался.
-                end_session: request.nlu.tokens.map(e => e.toLowerCase()).includes('хватит'),
+                text: chat(request.original_utterance, chatState),
+                end_session: request.nlu.tokens.map(toLowerCase).includes('хватит'),
             }
         }
     ));
