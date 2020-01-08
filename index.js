490a0f12a2ade4fc8b8bf00321512f42ade84366
hyrdbyrd <ilya777grin23@yandex-team.ru>
2020-01-08 15:29:24 +0300

    contries

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const { json } = require('micro');
 
+const { contries } = require('./lib/contries');
+
 module.exports = async (req, res) => {
     if (req.method !== 'POST') {
         res.statusCode = 404;
@@ -7,6 +9,12 @@ module.exports = async (req, res) => {
     }
 
     const { request, session, version } = await json(req);
+
+    const helloMessage =
+        'Привет! Я помогу тебе подтянуть твои знания в географии!' +
+        'Назови, пожалуйста, интересующий континент.' +
+        'Если тебе не важно какой какой континент учить, скажи "Земля"' +
+        'Если тебе надоест, просто скажи "Алиса, Хватит", и я тот час остановлюсь.';
 
     res.end(JSON.stringify(
         {
@@ -16,11 +24,12 @@ module.exports = async (req, res) => {
                 // В свойстве response.text возвращается исходная реплика пользователя.
                 // Если навык был активирован без дополнительной команды,
                 // пользователю нужно сказать "Hello!".
-                text: request.original_utterance || 'Йо!',
+                text:
+                    request.original_utterance || helloMessage,
 
                 // Свойство response.end_session возвращается со значением false,
                 // чтобы диалог не завершался.
-                end_session: false,
+                end_session: request.nlu.tokens.map(e => e.toLowerCase()).includes('хватит'),
             }
         }
     ));
